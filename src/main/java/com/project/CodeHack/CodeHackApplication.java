c3a9fcd7795550a0f1e2c9087844fe049cb6c63c
Rajat Sinha <111138210+Rajatsinha05@users.noreply.github.com>
2024-05-08 15:49:00 +0530

    initial setUp

---
+++ src/main/java/com/project/CodeHack/CodeHackApplication.java
@@ -0,0 +1,13 @@
+package com.project.CodeHack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeHackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/CodeHack/CodeHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.CodeHack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
